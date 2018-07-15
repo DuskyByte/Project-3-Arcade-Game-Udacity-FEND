ad5de53430512b7b647465325023794ee706ae7c
Samuel J. Smith-Thrasher <samuel.j.smith.thrasher@gmail.com>
2018-07-14 18:12:00 -0700

    feat: Tightened tolerances to make the game feel more natural

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 //TODO: Collapse classes into a main class and subclasses.
 //TODO: Have the correct subclasses inherit from the correct classes.
 //TODO: Add other functionalities like gems, scores, levels, etc...
+//class Sprite(location, speed) {
+//    
+//};
+
 let Enemy = function() {    
     //Stores the current row and column on board. Starting for all Enemies should be center brick row just off screen
     this.currentLocation = [-1, 2];
@@ -15,7 +19,7 @@ Enemy.prototype.update = function(dt) {
     //Prevents Enemies from scrolling off into eternity.
     if (this.currentLocation[0] >= 5) {
         this.currentLocation[0] = -1;
-    }
+    };
     //Updates currentLocation based on delta time and speed of Enemy.
     //TODO: Add ability for different enemy sprites has different speeds.
     this.currentLocation[0] = this.currentLocation[0] + dt;
@@ -32,14 +36,14 @@ let Player = function() {
     this.currentLocation = [2, 5];
     //Calculates drawing point for the sprite using currentLocation.
     this.x = (this.currentLocation[0] * 101);
-    this.y = ((this.currentLocation[1] * 83) - 10);
+    this.y = ((this.currentLocation[1] * 83) - 30);
     this.sprite = 'images/char-boy.png';
 };
 
-Player.prototype.update = function(dt) {
+Player.prototype.update = function() {
     //Updates x and y coordinates based on currentLocation.
     this.x = (this.currentLocation[0] * 101);
-    this.y = ((this.currentLocation[1] * 83) - 10);
+    this.y = ((this.currentLocation[1] * 83) - 30);
 };
 
 Player.prototype.render = function() {
@@ -49,16 +53,16 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(input) {
     if (input === 'left' && this.currentLocation[0] > 0) {
         this.currentLocation[0]--;
-    }
+    };
     if (input === 'right' && this.currentLocation[0] < 4) {
         this.currentLocation[0]++;
-    }
+    };
     if (input === 'up' && this.currentLocation[1] > 0) {
         this.currentLocation[1]--;
-    }
+    };
     if (input === 'down' && this.currentLocation[1] < 5) {
         this.currentLocation[1]++;
-    }
+    };
 };
 
 //TODO: Add functionality for additional Enemies to be added, based on difficulty.
@@ -78,13 +82,13 @@ document.addEventListener('keyup', function(input) {
 
 function checkCollisions() {
     //Checks if player is touching an Enemy.
-    if ((player.y - 10) === (allEnemies[0].y)) {
-        if((player.x) < (allEnemies[0].x + 81.81) && (player.x) > (allEnemies[0].x - 79.99)) {
+    if ((player.y + 30) === (allEnemies[0].y + 20)) {
+        if((player.x) < (allEnemies[0].x + 58) && (player.x) > (allEnemies[0].x - 58)) {
             player.currentLocation = [2, 5];
-        }
+        };
     }
     //Checks if player is in the water.
     if (player.currentLocation[1] === 0) {
         player.currentLocation = [2, 5];
-    }
-}
+    };
+};
